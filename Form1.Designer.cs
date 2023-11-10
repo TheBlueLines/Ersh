7e8806c054ccee36425999fd736b5d8e68dce412
TheBlueLines <davidfeher42@gmail.com>
2023-11-10 18:04:00 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Ersh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.source = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.location = new System.Windows.Forms.TextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // source
+            // 
+            this.source.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.source.Font = new System.Drawing.Font("Yu Gothic Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.source.Location = new System.Drawing.Point(12, 12);
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(548, 405);
+            this.source.TabIndex = 0;
+            this.source.Text = "";
+            // 
+            // location
+            // 
+            this.location.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.location.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.location.Location = new System.Drawing.Point(12, 423);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(466, 37);
+            this.location.TabIndex = 1;
+            // 
+            // save
+            // 
+            this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.save.BackColor = System.Drawing.Color.Green;
+            this.save.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.save.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.ForeColor = System.Drawing.SystemColors.Menu;
+            this.save.Location = new System.Drawing.Point(484, 423);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(76, 38);
+            this.save.TabIndex = 2;
+            this.save.Text = "SAVE";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 473);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.location);
+            this.Controls.Add(this.source);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ersh";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox source;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox location;
+        private System.Windows.Forms.Button save;
+    }
+}
+
